e87afd461db668bb122a85764ef2a23da443c7c1
Sandra Roman Rivera <sroman98@fb.com>
2019-06-27 10:29:31 -0700

    Initial Commit

---
+++ tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tumblr
+//
+//  Created by sroman98 on 6/27/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tumblrTests/tumblrTests.m
@@ -0,0 +1,37 @@
+//
+//  tumblrTests.m
+//  tumblrTests
+//
+//  Created by sroman98 on 6/27/19.
+//  Copyright © 2019 sroman98. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface tumblrTests : XCTestCase
+
+@end
+
+@implementation tumblrTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
